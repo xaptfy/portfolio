4c306858a08e3b9bfac0f44e2913482bc35eac97
Арина <xaptfy@MacBook-Pro-Arina.local>
2026-07-28 01:55:55 +0300

    Add Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import localFont from 'next/font/local'
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -14,8 +15,8 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Portfolio Arina Bykovskaya",
-  description: "Portfolio of Arina Bykovskaya, Product Designer",
+  title: "Portfolio Arina Bykovskaia",
+  description: "Portfolio of Arina Bykovskaia, Product Designer",
 };
 
 export default function RootLayout({
@@ -28,7 +29,7 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">{children} <Analytics /> </body>
     </html>
   );
 }
